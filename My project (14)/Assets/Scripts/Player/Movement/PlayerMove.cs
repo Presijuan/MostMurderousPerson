dce76f8c70c513011b6c2785de56a4e158afdd1e
Peter <pedropabloramirezarenas80@gmail.com>
2025-03-14 15:04:49 -0500

    Player Finished
    Lets goooooooooooooo

--- My project (14)/Assets/Scripts/Player/Movement/PlayerMove.cs
+++ My project (14)/Assets/Scripts/Player/Movement/PlayerMove.cs
@@ -28,14 +28,14 @@ public class newPlayerController : MonoBehaviour
     {
         isGrounded= Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if (isGrounded && velocity.y < 0) {
+        if (isGrounded && velocity.y < 0) 
+        {
             velocity.y = -2f;
-
         }
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x+ transform.forward*z ;
+        Vector3 move = transform.right * x + transform.forward * z ;
 
         controller.Move(move*speed*Time.deltaTime);
 
