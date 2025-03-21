e429e4f1474605e0d40d4d63836ea0c010acf561
Presijuan <juanmanuelhenaomedina@gmail.com>
2025-03-21 14:18:38 -0500

    MaxPoints

--- My project (14)/Assets/Scripts/GameOver/FinalScoreDisplay.cs
+++ My project (14)/Assets/Scripts/GameOver/FinalScoreDisplay.cs
@@ -1,13 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
 public class FinalScoreDisplay : MonoBehaviour
 {
+    [SerializeField] private TMP_Text highScoreText; // Texto del puntaje mas alto
+    int finalScore;                                  // Puntaje final ultima ronda
+
     void Start()
     {
-        int finalScore = ScoreGameOver.instance.GetScore(); // Obtiene el puntaje
-        GetComponent<TMP_Text>().text = finalScore + " Puntos";
+        finalScore = ScoreGameOver.instance.GetScore();         // Obtiene el puntaje
+         // Obtiene el puntaje
+        GetComponent<TMP_Text>().text = finalScore + " Puntos"; // Muestra el puntaje final
+        SetHighScore();                                         // Establece el puntaje mas alto
+    }
+
+    private void SetHighScore()
+    {
+        int highScore = PlayerPrefs.GetInt("HighScore", 0); // Obtiene el puntaje mas alto guardado
+
+        if(finalScore > highScore)                          // Si el puntaje final es mayor al puntaje mas alto
+        {
+            PlayerPrefs.SetInt("HighScore", finalScore);    // Guarda el puntaje final como el nuevo puntaje mas alto
+            highScore = finalScore;                         // Actualiza el puntaje mas alto
+        }
+
+        highScoreText.text = highScore + " Puntos Record";  // Muestra el puntaje mas alto
     }
 }
